6581eb57788420e92f6bd8c74cf4521415b1e58c
danielsoftdeleloper <daniel.andrade@sga.pucminas.br>
2021-03-10 21:50:47 -0300

    Instalando Nodemon

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const express = require('express');
+const app = express();
 
-const express = require('express')
-const app = express()
+app.listen(3000, function () {
+  console.log("server running on port 3000");
+});
+/* 
+app.get("/", (req, res) => {
+  res.send("A força está com você também!");
+}); */
 
-app.listen(3000, function(){
-    console.log('server running on port 3000')
-})
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('A força está com você')
-})
+  res.render('index.ejs');
+});
